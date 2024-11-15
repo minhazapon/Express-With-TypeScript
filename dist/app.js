a1b58eeef671d0194fb4f3376522ff71b2c1422d
minhazapon <minhajulabedinapon@gmail.com>
2024-11-15 04:58:40 -0800

    added file

--- dist/app.js
+++ dist/app.js
@@ -6,7 +6,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 5000;
+//parser 
+app.use(express_1.default.json());
+// const logger = (req: any, res: any, next: any) =>{
+//    console.log( req.url , req.method, req.hostName )
+//    next();
+// }
+const useRouter = express_1.default.Router();
+app.use('/', useRouter);
+useRouter.get('/api/D1', (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        name: '',
+        name1: 'minhazul abedin apon'
+    });
+});
 app.get('/', (req, res) => {
     res.send('express with typeScript!');
+    console.log(req.body);
+});
+app.post('/', (req, res) => {
+    res.send('Got Data');
+    console.log(req.body);
 });
 exports.default = app;
--- src/App.ts
+++ src/App.ts
@@ -2,8 +2,40 @@ import express from 'express'
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('express with typeScript!')
+
+//parser 
+app.use(express.json())
+
+  
+// const logger = (req: any, res: any, next: any) =>{
+//    console.log( req.url , req.method, req.hostName )
+//    next();
+// }
+
+const useRouter = express.Router()
+
+app.use('/', useRouter)
+
+useRouter.get('/api/D1',  (req: any , res: any) => {
+   const user = req.body 
+   console.log(user)
+   res.json({
+   name: '',
+   name1:'minhazul abedin apon'
+   })
 })
 
-export default app;
+app.get('/',  (req: any , res: any) => {
+  res.send('express with typeScript!')
+  console.log(req.body)
+})
+ 
+app.post('/', (req: any , res: any) => {
+  res.send('Got Data')
+  console.log(req.body)
+})
+
+
+
+
+export default app; 
